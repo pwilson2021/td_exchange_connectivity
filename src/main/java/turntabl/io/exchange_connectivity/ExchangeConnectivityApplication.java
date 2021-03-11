67843b9048bcb4702f9f3550e3e7a4a8eb789fce
pwilson2021 <philip.yirenkyi@turntabl.io>
2021-03-11 19:08:38 +0000

    setup gradle project

---
+++ src/main/java/turntabl/io/exchange_connectivity/ExchangeConnectivityApplication.java
@@ -0,0 +1,13 @@
+package turntabl.io.exchange_connectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeConnectivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/turntabl/io/exchange_connectivity/ExchangeConnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package turntabl.io.exchange_connectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeConnectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
