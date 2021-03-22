8c8ca8a6517814bd6c790f07acaf6e8440d402f9
pwilson2021 <philip.yirenkyi@turntabl.io>
2021-03-22 06:52:10 +0000

    added the different types of orders

--- src/main/java/turntabl/io/exchange_connectivity/ExchangeConnection.java
+++ src/main/java/turntabl/io/exchange_connectivity/ExchangeConnection.java
@@ -1,2 +1,20 @@
-package turntabl.io.exchange_connectivity;public class ExchangeConnection {
+package turntabl.io.exchange_connectivity;
+
+public class ExchangeConnection {
+
+    public void dynamicFetch(String ticker) {
+
+    }
+
+    public void createOrder() {
+
+    }
+
+    public void modifyOrder(){
+
+    }
+
+    public void cancelOrder(){
+
+    }
 }
